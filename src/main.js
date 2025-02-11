7b9ab8f435e68633a253a316af2f7fbeada91610
Malik Bennett <79369570+Rebrodie@users.noreply.github.com>
2025-02-11 18:08:46 -0500

    test commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,3 @@
 import './index.css'
+document.addEventListener("DOMContentLoaded", () => {
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 // const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main': {
+          50: "#EFE2E1",
+          100: "#E1C5C7",
+          200: "#D3A8AD",
+          300: "#C58B93",
+          400: "#B76E79",
+        },
+        'lavendar': '#F8E9E9',
+        'cherry': '#FFB3C1',
+      },
+    },
   },
   plugins: [],
 }
